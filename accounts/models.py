550bd350b6cb795587e42ea078967dccc392b204
Paul Istratoaie <p4ul.gg@gmail.com>
2021-08-14 06:09:06 +0100

    feat: update permissions for schools and add school one to one relationship to user (for teachers)

--- accounts/models.py
+++ accounts/models.py
@@ -36,6 +36,9 @@ class DigiCrecheUser(AbstractUser):
     county = models.CharField(max_length=80, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label="Country ", null=False, blank=False)
+    school = models.OneToOneField(
+        "schools.School", on_delete=models.DO_NOTHING, null=True,
+        blank=True, related_name='school')
 
     objects = CustomUserManager()
 
--- schools/api/views.py
+++ schools/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from schools.api.permissions import IsManagerOrReadOnly
 from schools.api.serializers import SchoolSerializer
 from schools.models import School
@@ -9,7 +9,7 @@ class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     lookup_field = 'slug'
     serializer_class = SchoolSerializer
-    permission_classes = [IsAuthenticated, IsManagerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsManagerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(manager=self.request.user)
