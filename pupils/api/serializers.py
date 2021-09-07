548912dbaae37d49284a541a1efd30099bfada25
Paul Istratoaie <p4ul.gg@gmail.com>
2021-09-07 22:50:56 +0100

    feat: write custom related fields to filter querysets for room and parents on pupil serializer

--- pupils/api/serializers.py
+++ pupils/api/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
 from pupils.models import Pupil
+from rooms.models import Room
+# from accounts.api.serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+
+class RoomRelatedField(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        queryset = Room.objects.filter(
+            school__slug=self.context['request'].resolver_match.kwargs.get(
+                'slug'))
+        return queryset
+
+
+class ParentRelatedField(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        queryset = get_user_model().objects.filter(
+            school__slug=self.context['request'].resolver_match.kwargs.get(
+                'slug'), user_type=3)
+        return queryset
 
 
 class PupilSerializer(serializers.ModelSerializer):
+    room = RoomRelatedField()
+    parents = ParentRelatedField(many=True)
 
     class Meta:
         model = Pupil
