696f91803eb23213ccb052760f56fd74f0f6b703
Paul Istratoaie <p4ul.gg@gmail.com>
2021-09-09 23:11:56 +0100

    feat: write custom permissions for activity and add admin permission to activity type
    Pupil's activities can be listed by its parent, its teacher or its school manager. It can be
    created, updated and deleted by its teacher or its school's manager.

---
+++ activities/api/permissions.py
@@ -0,0 +1,44 @@
+from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
+from schools.models import School
+from pupils.models import Pupil
+
+
+class IsSchoolManagerTeacherParentRUD(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        return request.user.is_authenticated and (
+            obj.pupil.school.manager == request.user or
+            obj.pupil.room.teacher == request.user or (
+                request.method in permissions.SAFE_METHODS and
+                request.user in obj.pupil.parents.all()
+            )
+        )
+
+
+class IsSchoolManagerTeacherParentSafe(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+
+        try:
+            kwarg_slug = request.parser_context['kwargs']['slug']
+            school = get_object_or_404(School, slug=kwarg_slug)
+
+            kwarg_pk = request.parser_context['kwargs']['pk']
+            pupil = get_object_or_404(Pupil, pk=kwarg_pk)
+        except Exception:
+            return False
+
+        return request.user.is_authenticated and (
+            school.manager == request.user or (
+                request.user.user_type == 2 and
+                request.user.school == school and
+                pupil.room.teacher == request.user
+            ) or (
+                request.method in permissions.SAFE_METHODS and
+                request.user.user_type == 3 and
+                request.user.school == school and
+                request.user in pupil.parents.all()
+            )
+        )
--- activities/api/views.py
+++ activities/api/views.py
@@ -1,19 +1,24 @@
 from rest_framework import generics, viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAdminUser
 from activities.api.serializers import (ActivitySerializer,
                                         ActivityTypeSerializer)
 from activities.models import Activity, ActivityType
 from pupils.models import Pupil
+from activities.api.permissions import (IsSchoolManagerTeacherParentSafe,
+                                        IsSchoolManagerTeacherParentRUD)
 
 
 class ActivityTypeViewSet(viewsets.ModelViewSet):
     queryset = ActivityType.objects.all()
     serializer_class = ActivityTypeSerializer
+    permission_classes = [IsAdminUser]
 
 
 class ActivityListCreateAPIView(generics.ListCreateAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    permission_classes = [IsSchoolManagerTeacherParentSafe]
 
     def perform_create(self, serializer):
         kwarg_pk = self.kwargs.get('pk')
@@ -30,3 +35,4 @@ class ActivityListCreateAPIView(generics.ListCreateAPIView):
 class ActivityRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    permission_classes = [IsSchoolManagerTeacherParentRUD]
