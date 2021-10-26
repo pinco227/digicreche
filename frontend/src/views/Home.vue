1cb9c39f8a4c221b71a011d126f57740dcf7c85e
Paul Istratoaie <p4ul.gg@gmail.com>
2021-10-26 12:54:42 +0100

    fix: fIX TypeError and page block on blank by fetching user on home and switching user_type after

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,36 +1,45 @@
 <template><section id="home"></section></template>
 
 <script>
+import { apiService } from "@/common/api.service.js";
 export default {
   name: "Home",
+  methods: {
+    async getUser() {
+      // Fetches the user data from API
+      const data = await apiService("/api/rest-auth/user/");
+      if (data.status >= 200 && data.status < 300) {
+        const user = data.body;
+        const schoolSlug = user.school_slug;
+        const roomId = user.room;
+        const user_type = user.user_type;
+        switch (user_type) {
+          case 1:
+            this.$router.push({ name: "manager-schools" });
+            break;
+          case 2:
+            if (schoolSlug && roomId)
+              this.$router.push(`/schools/${schoolSlug}/${roomId}`);
+            else this.$router.push({ name: "user_account" });
+            break;
+          case 3:
+            this.$router.push({ name: "parent-children" });
+            break;
+
+          default:
+            this.$router.push({ name: "user_account" });
+            break;
+        }
+      } else {
+        if (Object.prototype.hasOwnProperty.call(data.body, "detail")) {
+          this.$toast.error(data.body.detail);
+        }
+        if (data.status == 403) this.updatePermission(false);
+      }
+    },
+  },
   created() {
-    const user = JSON.parse(window.localStorage.getItem("user"));
-    let schoolSlug = null;
-    let roomId = null;
-    let user_type = null;
-    if (Object.keys(user).length) {
-      schoolSlug = user.school_slug;
-      roomId = user.room;
-      user_type = user.user_type;
-    }
-
-    switch (user_type) {
-      case 1:
-        this.$router.push({ name: "manager-schools" });
-        break;
-      case 2:
-        if (schoolSlug && roomId)
-          this.$router.push(`/schools/${schoolSlug}/${roomId}`);
-        else this.$router.push({ name: "user_account" });
-        break;
-      case 3:
-        this.$router.push({ name: "parent-children" });
-        break;
-
-      default:
-        this.$router.push({ name: "user_account" });
-        break;
-    }
+    this.getUser();
   },
 };
 </script>
